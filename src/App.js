9deb462e2300d3a64b713a26eba26297c917c31e
Enrique Barco <enrique.barco96@hotmail.com>
2022-02-15 12:17:54 -0500

    added react routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from './pages/LandingPage/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="app">
+                <BrowserRouter>
+                  <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                  </Routes>
+                </BrowserRouter>
+            </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class LandingPage extends Component {
+  render() {
+    return (
+      <div>This is my landing page</div>
+    )
+  }
+}
+
+export default LandingPage
