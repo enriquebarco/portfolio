2048cf2dcd316b5d4d171462a5aed0d4e8d67d09
Enrique Barco <enrique.barco96@hotmail.com>
2022-02-16 17:00:04 -0500

    cleaned up header code

--- src/components/LandingHero/LandingHero.jsx
+++ src/components/LandingHero/LandingHero.jsx
@@ -5,7 +5,7 @@ import "./LandingHero.scss";
 export default function LandingHero() {
   return (
     <section className='landing-hero'>
-        <h1 className="landing-hero__title">Entrepreneur, Full-Stack Developer & Adventurer</h1>
+        <h1 className="landing-hero__title">Entrepreneur, Full-Stack Developer, & Adventurer</h1>
         <h2 className="landing-hero__text"> I bring products to life, from zero to one. I don't stop until an idea becomes reality, and I love what I do</h2>
         <img className="landing-hero__image" src={Image} alt="Enrique Barco Kite Surf" />
     </section>
--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -20,14 +20,28 @@ export class PageHeader extends Component {
           <img className="header__logo" src={Logo} alt="logo placeholder" />
           <div className="header__button-area">
             <label className="header__button-label" htmlFor="check" >
-              <input type="checkbox" id="check"  onClick={this.handleClick}/> 
+              <input className="header__input" type="checkbox" id="check"  onClick={this.handleClick}/> 
               <span className='header__button-bar header__button-bar--1'></span>
               <span className='header__button-bar header__button-bar--2'></span>
               <span className='header__button-bar header__button-bar--3'></span>
             </label>
           </div>
         </div>
-        {this.state.isOpen ? <button>Get in Touch</button> : ""}
+        {this.state.isOpen ? 
+        <div className="header__hidden-CTA">
+          <div className="header__hidden-container">
+            <a href="https://github.com/enriquebarco" className="header__media-links">
+              <img src="" alt="github icon" className="header__media-icons" />
+            </a>
+            <a href="https://www.linkedin.com/in/enriquebarco/" className="header__media-links">
+              <img src="" alt="linkedin icon" className="header__media-icons" />
+            </a>
+            <a href="https://www.instagram.com/kikebarco/" className="header__media-links">
+              <img src="" alt="instagram icon" className="header__media-icons" />
+            </a>
+          </div>
+          <button>Get in Touch</button>
+        </div> : ""}
     </header>
     )
   }
