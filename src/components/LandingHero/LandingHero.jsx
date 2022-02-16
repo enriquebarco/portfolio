7e5553946c69335f113f16e229a03570e3d3ed04
Enrique Barco <enrique.barco96@hotmail.com>
2022-02-16 15:41:42 -0500

    added font

--- src/components/LandingHero/LandingHero.jsx
+++ src/components/LandingHero/LandingHero.jsx
@@ -7,7 +7,7 @@ export default function LandingHero() {
     <section className='landing-hero'>
         <h1 className="landing-hero__title">Entrepreneur, Full-Stack Developer & Adventurer</h1>
         <h2 className="landing-hero__text"> I bring products to life, from zero to one. I don't stop until an idea becomes reality, and I love what I do</h2>
-        <img src={Image} alt="Enrique Barco Kite Surf" />
+        <img className="landing-hero__image" src={Image} alt="Enrique Barco Kite Surf" />
     </section>
   )
 }
