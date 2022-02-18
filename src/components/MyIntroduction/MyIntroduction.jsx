fe3ec3674439099c703b3636c5c4a4444e78f3de
Enrique Barco <enrique.barco96@hotmail.com>
2022-02-18 14:24:32 -0500

    added introduction

--- src/components/MyIntroduction/MyIntroduction.jsx
+++ src/components/MyIntroduction/MyIntroduction.jsx
@@ -4,8 +4,8 @@ import "./MyIntroduction.scss";
 export default function MyIntroduction() {
   return (
     <section className="introduction">
-        <h3 className="introduction__title">Hi i'm Kique, nice to meet you.</h3>
-        <p className="introduction__text">My journey as a full stack developer began when I launched my first business and built the product on a no-code platform. I fell in love with the act of creating, and jumped head first into the world of Full Stack Development. Since the completion of my bootcamp with BrainStation, I have completed a number of projects which I would love to share with you, find out more below!</p>
+        <h3 className="introduction__title">Hi i'm Enrique, nice to meet you.</h3>
+        <p className="introduction__text">My journey as a full stack developer began when I launched my first business and I built the product on a no-code platform. I fell in love with the act of creating, and jumped head first into the world of development. Since the completion of my boot camp with BrainStation, I have worked on a number of projects which I would love to share with you, find out more below!</p>
     </section>
   )
 }
