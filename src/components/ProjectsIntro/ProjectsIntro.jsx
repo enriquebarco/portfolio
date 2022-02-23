d50c47a55c716eb3f14239218ee6c66e6e12c676
Enrique Barco <enrique.barco96@hotmail.com>
2022-02-23 18:23:08 -0500

    added more information to project info

--- src/components/ProjectsIntro/ProjectsIntro.jsx
+++ src/components/ProjectsIntro/ProjectsIntro.jsx
@@ -5,8 +5,8 @@ export default function ProjectsIntro() {
   return (
     <div className="projects-intro">
         <h3 className="projects-intro__title">My Recent Work</h3>
-        <p className="projects-intro__text">Here are a few of the projects I have worked on. Curious to learn more?
-            <a href="mailto:enrique.barco96@hotmail.com" className="project-intro__link">Email me.</a>
+        <p className="projects-intro__text">Here are a few of the projects I have worked on. Curious to learn more? 
+            <a href="mailto:enrique.barco96@hotmail.com" className="project-intro__link"> Email me.</a>
         </p>
     </div>
   )
