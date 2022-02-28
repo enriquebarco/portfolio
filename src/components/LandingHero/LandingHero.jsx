95edef447bc77ea85de7c64c527d921b18c5461c
Enrique Barco <enrique.barco96@hotmail.com>
2022-02-28 10:27:22 -0500

    added CTA link on landing page

--- src/components/LandingHero/LandingHero.jsx
+++ src/components/LandingHero/LandingHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Image from "../../assets/images/kitesurf2.jpeg";
 import Decoration from "../../assets/images/desk.webp";
 import 'animate.css';
@@ -9,6 +10,7 @@ export default function LandingHero() {
     <section className='landing-hero animate__animated animate__fadeInUpBig'>
         <h1 className="landing-hero__title">Entrepreneur, Full-Stack Developer, & Adventurer</h1>
         <h2 className="landing-hero__text "> I bring products to life, from zero to one. I don't stop until an idea becomes reality, and I love what I do</h2>
+        <Link className="landing-hero__link" to="/contact">Get in touch</Link>
         <img className="landing-hero__image " src={Image} alt="Enrique Barco Kite Surf" />
         <img src={Decoration} alt="decoration icon" className="landing-hero__decoration-image" />
     </section>
