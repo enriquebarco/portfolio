c4e1bac6a9687a1bc4e591c3540b75d530c6e337
Enrique Barco <enrique.barco96@hotmail.com>
2022-11-03 00:54:27 -0400

    minor adjustments to index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import {useEffect} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import "./index.scss";
 
-function AppWithCallbackAfterRender() {
-  useEffect(() => {
-    console.log('rendered');
-  });
-
-  return <App tab="home" />
-}
-
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<AppWithCallbackAfterRender />);
+root.render(<App tab="home" />);
