f69ea5df8360275e8da1783564fe5fb10dcad7f3
Enrique Barco <enrique.barco96@hotmail.com>
2022-11-03 00:52:39 -0400

    updated react and maintined

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {useEffect} from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import "./index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function AppWithCallbackAfterRender() {
+  useEffect(() => {
+    console.log('rendered');
+  });
 
+  return <App tab="home" />
+}
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<AppWithCallbackAfterRender />);
