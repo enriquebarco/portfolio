d868b1a3fc1fda291956c450952e06066dd5bcb0
Enrique Barco <enrique.barco96@hotmail.com>
2022-11-03 01:35:08 -0400

    save point

--- src/components/Three/Three.jsx
+++ src/components/Three/Three.jsx
@@ -1,16 +1,33 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { MeshStandardMaterial } from 'three';
+import { PerspectiveCamera } from '@react-three/drei';
+
+const angleToRadians = (angleInDeg) => (Math.PI / 180) * angleInDeg;
+
+console.log(PerspectiveCamera)
 
 
 export default function Three() {
   return (
     <Canvas style={{ height: '100vh', background: 'black' }}>
       <Suspense fallback={<></>}>
+        {/* camera */}
+        <PerspectiveCamera makeDefault position={[0, 1, 5]} />
+
+        {/* sphere */}
         <mesh>
-          <sphereGeometry args={[1, 32, 32]} />
-          <MeshStandardMaterial color="white" />
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial color="white" />
         </mesh>
+
+        {/* floor */}
+        <mesh rotation={[ (angleToRadians(90)), 0, 0]}>
+          <planeGeometry args={[7, 7]}/>
+          <meshStandardMaterial color="blue" />
+        </mesh> 
+
+        {/* light */}
+        <ambientLight args={['white', 1]} />
       </Suspense>
     </Canvas>
   )
